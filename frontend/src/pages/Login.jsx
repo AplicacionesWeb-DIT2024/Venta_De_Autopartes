73d79e2a9a56ba17f1c8ae48e34664a0c1aecded
FrancoDeMarco <franco92tw@gmail.com>
2026-04-20 10:04:56 -0300

    Trabajando en la estética con Bootstrap

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,25 @@
 //Frontend del Login
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const [errorMsg, setErrorMsg] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    
+    setErrorMsg(''); // Limpiar mensajes de error anteriores
+
     try {
-      const response = await axios.post('http://localhost:8000/api/login', { 
-        email: username, 
-        password 
+      const response = await axios.post('http://localhost:8000/api/login', {
+        email: username,
+        password
       });
-      
+
       // Guardar el usuario y token en localStorage
       localStorage.setItem('user', JSON.stringify({
         name: response.data.user.name,
@@ -24,43 +27,77 @@ const Login = () => {
         role: response.data.user.role
       }));
       localStorage.setItem('token', response.data.token);
-      
-      navigate('/autoparts'); // Redirige a la página de autopartes después del login exitoso 
-    
+
+      navigate('/autoparts'); // Redirige a la página de autopartes después del login exitoso
+
     } catch (error) {
-      console.error('Error during login:', error);
-      alert(error.response?.data?.message || 'Error al iniciar sesión'); // Muestra un mensaje de error al usuario
+      setErrorMsg(error.response?.data?.message || 'Error al iniciar sesión'); // Muestra un mensaje de error al usuario
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="text"
-            value={username} // 
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <div className="container">
+      <div className="row justify-content-center" style={{ marginTop: '80px' }}>
+        <div className="col-md-6" col-lg-5>
+
+          <div className="card shadow">
+            <div className="card-header text-center">
+              <h2>Login</h2>
+            </div>
+
+            <div className="card-body">
+              <form onSubmit={handleLogin}>
+
+                {/* ERROR */}
+                {errorMsg && (
+                  <div className="alert alert-danger">
+                    {errorMsg}
+                  </div>
+                )}
+
+                {/* EMAIL */}
+                <div className="mb-3">
+                  <label className="form-label">Email address:</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    autoFocus
+                  />
+                </div>
+
+                {/* PASSWORD */}
+                <div className="mb-3">
+                  <label className="form-label">Password:</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+
+                {/* BOTONES */}
+                <div className="d-flex justify-content-between align-items-center">
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
+
+                  <Link to="/register" className="btn btn-link">
+                    Registrarse
+                  </Link>
+                </div>
+
+              </form>
+            </div>
+          </div>
+
+
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-        <p>
-          {/* Agrega un enlace para registrarse si el usuario no tiene una cuenta */}
-          No tenés cuenta? <a href="/register">Register here</a>
-        </p>
-      </form>
+      </div>
     </div>
   );
 }
