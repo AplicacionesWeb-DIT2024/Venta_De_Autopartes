76117df75dedca45c2eb9318dddc0781040e014f
FrancoDeMarco <franco92tw@gmail.com>
2026-07-27 17:51:44 -0300

    Ya se elimina la autoparte del listado de autopartes cuando el stock es 0.

--- backend/app/Http/Controllers/Api/CompraController.php
+++ backend/app/Http/Controllers/Api/CompraController.php
@@ -9,6 +9,7 @@ use App\Models\Autopart;
 use App\Models\Pedido;
 use App\Models\DetallePedido;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CompraController extends Controller
 {
@@ -19,65 +20,74 @@ class CompraController extends Controller
             'forma_pago' => 'required|string'
         ]);
 
-        $carritoItems = Carrito::where('user_id', Auth::id())
-            ->with('autopart')
-            ->get();
+        return DB::transaction(function () use ($request) {
 
-        if ($carritoItems->isEmpty()) {
-            return response()->json([
-                'message' => 'El carrito está vacío'
-            ], 400);
-        }
+            $carritoItems = Carrito::where('user_id', Auth::id())
+                ->with('autopart')
+                ->get();
 
-        $total = 0;
-
-        foreach ($carritoItems as $item) {
-            $total += $item->autopart->precio * $item->stock;
-        }
-        //El nombre de las columnas del pedido
-        $pedido = Pedido::create([
-            'user_id' => Auth::id(),
-            'fecha_cierre' => now(),
-            'costo_total' => $total,
-            'tipo_pago' => $request->forma_pago,
-        ]);
-
-        foreach ($carritoItems as $item) {
-            DetallePedido::create([
-                'pedido_id' => $pedido->id,
-                'autoparte' => $item->autopart->autoparte,
-                'marca' => $item->autopart->marca,
-                'modelo' => $item->autopart->modelo,
-                'codigo' => $item->autopart->codigo,
-                'precio' => $item->autopart->precio,
-                'cantidad' => $item->stock,
-            ]);
-
-            // Descontar stock
-            $autopart = Autopart::find($item->autopart_id);
-
-            if ($autopart) {
-
-                if ($autopart->stock < $item->stock) {
-                    return response()->json([
-                        'message' => 'Stock insuficiente para ' . $autopart->autoparte
-                    ], 400);
-                }
-
-                $autopart->stock -= $item->stock;
-                $autopart->save();
+            if ($carritoItems->isEmpty()) {
+                return response()->json([
+                    'message' => 'El carrito está vacío'
+                ], 400);
             }
 
-        }
+            $total = 0;
+
+            foreach ($carritoItems as $item) {
+                $total += $item->autopart->precio * $item->stock;
+            }
+            //El nombre de las columnas del pedido
+            $pedido = Pedido::create([
+                'user_id' => Auth::id(),
+                'fecha_cierre' => now(),
+                'costo_total' => $total,
+                'tipo_pago' => $request->forma_pago,
+            ]);
+
+            foreach ($carritoItems as $item) {
+
+                DetallePedido::create([
+                    'pedido_id' => $pedido->id,
+                    'autoparte' => $item->autopart->autoparte,
+                    'marca' => $item->autopart->marca,
+                    'modelo' => $item->autopart->modelo,
+                    'codigo' => $item->autopart->codigo,
+                    'precio' => $item->autopart->precio,
+                    'cantidad' => $item->stock,
+                ]);
+
+                // Descontar stock
+                $autopart = Autopart::find($item->autopart_id);
+
+                if ($autopart) {
+
+                    if ($autopart->stock < $item->stock) {
+                        return response()->json([
+                            'message' => 'Stock insuficiente para ' . $autopart->autoparte
+                        ], 400);
+                    }
+
+                    $autopart->decrement('stock', $item->stock);
+
+                    $autopart->refresh();
+
+                    if ($autopart->stock <= 0) {
+                        $autopart->delete(); // Si el stock de autoparte llega a 0, se se elimina la autoparte de la BD.
+                    }
+
+                }
+
+            }
 
 
-        //Vaciar carrito
-        Carrito::where('user_id', Auth::id())->delete();
+            //Vaciar carrito
+            Carrito::where('user_id', Auth::id())->delete();
 
-        return response()->json([
-            'message' => 'Compra procesada con éxito',
-            'pedido_id' => $pedido->id
-        ]);
+            return response()->json([
+                'message' => 'Compra procesada con éxito',
+                'pedido_id' => $pedido->id
+            ]);
+        });
     }
-
 }
