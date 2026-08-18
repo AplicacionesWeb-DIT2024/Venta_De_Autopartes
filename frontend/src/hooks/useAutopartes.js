28100719324369d511caef14474412f57bede097
FrancoDeMarco <franco92tw@gmail.com>
2026-08-18 10:38:33 -0300

    Corregido el bug del Eliminar

--- frontend/src/hooks/useAutopartes.js
+++ frontend/src/hooks/useAutopartes.js
@@ -53,12 +53,12 @@ export const useAutopartes = () => {
 
   const deleteAutoparte = async (id) => {
     try {
-      await api.delete(`/api/autoparts/${id}`);
-      
-      setAutopartes( prev =>
+      await api.delete(`/autoparts/${id}`);
+
+      setAutopartes(prev =>
         prev.filter(autopart => autopart.id !== id)
       );
-    
+
     } catch (err) {
       console.error('Error al eliminar autoparte:', err);
       setError(err);
